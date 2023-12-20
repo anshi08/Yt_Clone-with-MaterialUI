73b5a32a3a549a6f1c2b378d91f965ef01aaf3bc
anshi08 <sabharwalanshika08@gmail.com>
2023-12-20 13:57:18 -0800

    Done final Commit

--- src/components/Videos.jsx
+++ src/components/Videos.jsx
@@ -3,11 +3,11 @@ import { Stack, Box } from "@mui/material";
 import VideoCard from './VideoCard';
 import ChannelCard from './ChannelCard';
 
-const Videos = ({videos}) => {
+const Videos = ({videos , direction}) => {
 
   // console.log("cc",videos)
   return (
-    <Stack direction="row" flexWrap="wrap" justifyContent="start" alignItems="start" gap={2}>
+    <Stack direction = {direction || "row"} flexWrap="wrap" justifyContent="start" alignItems="start" gap={2}>
     {
       videos.map((item,idx) =>(
         <Box key={idx}>
